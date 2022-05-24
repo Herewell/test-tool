ef69f70f4492a5a3169526c5e7345177137035d5
Herewell <53120626+Herewell@users.noreply.github.com>
2022-05-24 23:30:24 +0800

    rocketMQ

---
+++ src/main/java/com/example/testtool/mq/rocketmq/ConsumerC3.java
@@ -0,0 +1,47 @@
+/**
+ * Author:   Herewe
+ * Date:     2022/4/4 1:02
+ * Description:
+ */
+package com.example.testtool.mq.rocketmq;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
+
+import java.util.List;
+
+public class ConsumerC3 {
+    public static void main(String[] args) throws InterruptedException, MQClientException {
+
+        // Instantiate with specified consumer group name.
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("consumerGroup2");
+
+        // Specify name server addresses.
+        consumer.setNamesrvAddr("114.132.232.241:9876");
+
+        // Subscribe one more more topics to consume.
+        consumer.subscribe("topicTest1", "*");
+        consumer.setMessageModel(MessageModel.CLUSTERING);
+        // Register callback to execute on arrival of messages fetched from brokers.
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                                                            ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        //Launch the consumer instance.
+        consumer.start();
+
+        System.out.printf("Consumer Started.%n");
+    }
+
+}
---
+++ src/main/java/com/example/testtool/mq/rocketmq/Producer.java
@@ -0,0 +1,36 @@
+/**
+ * Author:   Herewe
+ * Date:     2022/4/4 1:02
+ * Description:
+ */
+package com.example.testtool.mq.rocketmq;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+public class Producer {
+    public static void main(String[] args) throws Exception {
+        //Instantiate with a producer group name.
+        DefaultMQProducer producer = new
+                DefaultMQProducer("producerGroup1");
+        // Specify name server addresses.
+        producer.setNamesrvAddr("114.132.232.241:9876");
+        //Launch the instance.
+        producer.start();
+        for (int i = 0; i < 1; i++) {
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("topicTest1" /* Topic */,
+                    "TagA" /* Tag */,
+                    ("Hello RocketMQ " +
+                            i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            //Call send message to deliver message to one of brokers.
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+        //Shut down once the producer instance is not longer in use.
+        producer.shutdown();
+    }
+}
