ca6f979138f363f93ae5fc4e67455b178d3f9aa6
chengdu <chengdu0110@gmail.com>
2023-03-19 18:12:53 +0800

    字符串更新

--- src/main/java/com/example/jvm/StringPoolTest.java
+++ src/main/java/com/example/jvm/StringPoolTest.java
@@ -5,9 +5,14 @@ package com.example.jvm;
  */
 public class StringPoolTest {
 	public static void main(String[] args) {
+		String b1 = new StringBuilder("a").append("bc").toString();// 不会放入字符串常量池
+		String q1 = "abc"; // 会放到常量池并返回
+		System.out.println(b1 == q1); // false
+		System.out.println(b1.intern() == q1); // true
+
 		String str1 = new StringBuilder("计算机").append("软件").toString();
 		System.out.println(str1.intern() == str1); // 1.7之前是false，之后是true
-		String str2 = new String("哈哈哈");
+		String str2 = new String("哈哈哈"); // 并生成两个对象，一个是字符串常量池中，一个是堆中
 		System.out.println(str2.intern() == str2); // false
 		String str3 = "这个是一个测试";
 		System.out.println(str3.intern() == str3); // true。
@@ -28,6 +33,9 @@ public class StringPoolTest {
 		System.out.println(s1 == s4); // true
 		System.out.println(s1 == s5); // false
 		System.out.println(s1 == s6); // false
+		System.out.println(s4 == s5); // false
+		System.out.println(s4 == s6); // false
+		System.out.println(s6 == s5); // false
 
 	}
 }
